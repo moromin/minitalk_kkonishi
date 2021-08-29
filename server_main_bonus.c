69a081e6c10f5ac74dbd7c4a057db5da548c2163
moromin <34294957+moromin@users.noreply.github.com>
2021-08-29 15:41:58 +0900

    modify counter

--- server_main_bonus.c
+++ server_main_bonus.c
@@ -9,35 +9,41 @@ void	clear_buf(unsigned char *buf)
 		buf[i++] = 0;
 }
 
+void	print_message(unsigned char *buf, size_t *itr)
+{
+	size_t	size;
+
+	size = *itr;
+	write(1, buf, size + 1);
+	*itr = 0;
+}
+
 void	server_handler(int signo, siginfo_t *info, void *context)
 {
 	static unsigned char	uc;
 	static int				i;
 	static int				count;
 	static unsigned char	buf[BUFSIZ];
+	static size_t			itr;
 
 	(void)context;
 	if (count % 8 == 0)
-	{
 		uc = 0;
+	if (count % 8 == 0)
 		i = 1;
-	}
 	else
 		i *= 2;
 	count++;
 	if (signo == SIGUSR2)
 		uc += i;
 	if (count % 8 == 0)
-		buf[count / 8 - 1] = uc;
+		buf[itr++] = uc;
 	if (count % 8 == 0 && (count / 8 == BUFSIZ - 1 || uc == 0))
-		write(1, buf, count / 8);
+		print_message(buf, &itr);
 	if (count / 8 == BUFSIZ - 1)
 		clear_buf(buf);
 	if (kill(info->si_pid, SIGUSR1) == -1)
-	{
-		write(1, "ACK sending error\n", 18);
 		exit(EXIT_FAILURE);
-	}
 }
 
 int	main(void)
