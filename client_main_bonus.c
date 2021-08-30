f03b646af6f2b9512a3cbdcd6154f2780311ad56
moromin <34294957+moromin@users.noreply.github.com>
2021-08-30 21:09:44 +0900

    string buffering

--- client_main_bonus.c
+++ client_main_bonus.c
@@ -1,6 +1,79 @@
-#include "minitalk.h"
+// #include "minitalk.h"
 
-volatile int	g_receive_signal;
+// volatile int	g_receive_signal;
+
+// void	args_check(int argc, char *argv[])
+// {
+// 	pid_t	pid;
+
+// 	if (argc != 3)
+// 	{
+// 		write(2, "Error\n", 6);
+// 		write(2, "Usage: ./client [PID] [string]\n", 31);
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	pid = (pid_t)ft_atoi(argv[1]);
+// 	if (pid <= 0 || kill(pid, 0) == -1)
+// 	{
+// 		write(2, "Error\n", 6);
+// 		write(2, "Invalid PID.\n", 13);
+// 		exit(EXIT_FAILURE);
+// 	}
+// }
+
+// void	send_char(pid_t pid, char c)
+// {
+// 	int				bit;
+// 	int				i;
+// 	unsigned char	uc;
+
+// 	uc = (unsigned char)c;
+// 	i = 0;
+// 	while (i < 8)
+// 	{
+// 		usleep(50);
+// 		g_receive_signal = 0;
+// 		bit = (uc >> i) & 0x01;
+// 		if (kill(pid, SIGUSR1 + bit) == -1)
+// 		{
+// 			write(2, "sending char error\n", 19);
+// 			exit(EXIT_FAILURE);
+// 		}
+// 		while (g_receive_signal != SIGUSR1)
+// 			;
+// 		i++;
+// 	}
+// }
+
+// void	client_handler(int signo, siginfo_t *info, void *context)
+// {
+// 	(void)info;
+// 	(void)context;
+// 	g_receive_signal = signo;
+// }
+
+// int	main(int argc, char *argv[])
+// {
+// 	struct sigaction	act2;
+// 	size_t				i;
+// 	size_t				size;
+
+// 	args_check(argc, argv);
+// 	act2.sa_sigaction = client_handler;
+// 	act2.sa_flags = SA_SIGINFO;
+// 	if (sigaction(SIGUSR1, &act2, NULL) != 0)
+// 	{
+// 		write(2, "sigcation error\n", 16);
+// 		return (1);
+// 	}
+// 	size = ft_strlen(argv[2]);
+// 	i = 0;
+// 	while (i < size + 1)
+// 		send_char((pid_t)ft_atoi(argv[1]), argv[2][i++]);
+// 	return (0);
+// }
+
+#include "minitalk.h"
 
 void	args_check(int argc, char *argv[])
 {
@@ -31,17 +104,16 @@ void	send_char(pid_t pid, char c)
 	i = 0;
 	while (i < 8)
 	{
-		usleep(50);
-		g_receive_signal = 0;
 		bit = (uc >> i) & 0x01;
 		if (kill(pid, SIGUSR1 + bit) == -1)
 		{
 			write(2, "sending char error\n", 19);
 			exit(EXIT_FAILURE);
 		}
-		while (g_receive_signal != SIGUSR1)
-			;
 		i++;
+		while (g_signal == 0)
+			;
+		g_signal = 0;
 	}
 }
 
@@ -49,7 +121,8 @@ void	client_handler(int signo, siginfo_t *info, void *context)
 {
 	(void)info;
 	(void)context;
-	g_receive_signal = signo;
+	if (signo == SIGUSR1)
+		g_signal = SIGUSR1;
 }
 
 int	main(int argc, char *argv[])
@@ -59,8 +132,9 @@ int	main(int argc, char *argv[])
 	size_t				size;
 
 	args_check(argc, argv);
-	act2.sa_sigaction = client_handler;
+	act2.sa_sigaction = &client_handler;
 	act2.sa_flags = SA_SIGINFO;
+	g_signal = 0;
 	if (sigaction(SIGUSR1, &act2, NULL) != 0)
 	{
 		write(2, "sigcation error\n", 16);
--- server_main_bonus.c
+++ server_main_bonus.c
@@ -1,70 +1,149 @@
+// #include "minitalk.h"
+
+// void	clear_buf(unsigned char *buf)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (i < BUFSIZ)
+// 		buf[i++] = 0;
+// }
+
+// void	print_message(unsigned char *buf, size_t *itr)
+// {
+// 	size_t	size;
+
+// 	size = *itr;
+// 	write(1, buf, size);
+// 	*itr = 0;
+// }
+
+// void	server_handler(int signo, siginfo_t *info, void *context)
+// {
+// 	static unsigned char	uc;
+// 	static int				i;
+// 	static int				count;
+// 	static unsigned char	buf[BUFSIZ];
+// 	static size_t			itr;
+
+// 	(void)context;
+// 	if (count % 8 == 0)
+// 		uc = 0;
+// 	if (count % 8 == 0)
+// 		i = 1;
+// 	else
+// 		i *= 2;
+// 	count++;
+// 	if (signo == SIGUSR2)
+// 		uc += i;
+// 	if (count % 8 == 0)
+// 		buf[itr++] = uc;
+// 	if (count % 8 == 0 && (itr == BUFSIZ - 1 || uc == 0))
+// 		print_message(buf, &itr);
+// 	if (itr == BUFSIZ - 1)
+// 		clear_buf(buf);
+// 	if (kill(info->si_pid, SIGUSR1) == -1)
+// 		exit(EXIT_FAILURE);
+// }
+
+// int	main(void)
+// {
+// 	struct sigaction	act1;
+
+// 	act1.sa_sigaction = server_handler;
+// 	act1.sa_flags = SA_SIGINFO;
+// 	write(1, "server PID: ", 12);
+// 	ft_putnbr_fd(getpid(), 1);
+// 	if (sigaction(SIGUSR1, &act1, NULL) != 0)
+// 	{
+// 		write(2, "sigcation error\n", 16);
+// 		return (1);
+// 	}
+// 	if (sigaction(SIGUSR2, &act1, NULL) != 0)
+// 	{
+// 		write(2, "sigcation error\n", 16);
+// 		return (1);
+// 	}
+// 	while (1)
+// 		pause();
+// 	return (0);
+// }
+
 #include "minitalk.h"
-
-void	clear_buf(unsigned char *buf)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < BUFSIZ)
-		buf[i++] = 0;
-}
-
-void	print_message(unsigned char *buf, size_t *itr)
-{
-	size_t	size;
-
-	size = *itr;
-	write(1, buf, size);
-	*itr = 0;
-}
 
 void	server_handler(int signo, siginfo_t *info, void *context)
 {
+	(void)context;
+	if (signo == SIGUSR1)
+		g_signal = info->si_pid;
+	if (signo == SIGUSR2)
+		g_signal = -1 * info->si_pid;
+}
+
+static void	print_buffer(unsigned char *str, size_t *itr)
+{
+	size_t	size;
+	size_t	i;
+
+	size = *itr;
+	write(1, str, size);
+	i = 0;
+	while (i < BUFSIZ)
+		str[i++] = 0;
+	*itr = 0;
+}
+
+static void	server_loop(void)
+{
 	static unsigned char	uc;
-	static int				i;
 	static int				count;
 	static unsigned char	buf[BUFSIZ];
 	static size_t			itr;
 
-	(void)context;
-	if (count % 8 == 0)
-		uc = 0;
-	if (count % 8 == 0)
-		i = 1;
-	else
-		i *= 2;
+	while (g_signal == 0)
+		;
+	if (g_signal > 0)
+		uc &= ~(1 << count);
+	if (g_signal < 0)
+		uc |= (1 << count);
 	count++;
-	if (signo == SIGUSR2)
-		uc += i;
 	if (count % 8 == 0)
+	{
 		buf[itr++] = uc;
-	if (count % 8 == 0 && (itr == BUFSIZ - 1 || uc == 0))
-		print_message(buf, &itr);
-	if (itr == BUFSIZ - 1)
-		clear_buf(buf);
-	if (kill(info->si_pid, SIGUSR1) == -1)
-		exit(EXIT_FAILURE);
+		uc = 0;
+	}
+	if (count % 8 == 0 && (uc == 0 || itr == BUFSIZ))
+	{
+		print_buffer(buf, &itr);
+		count = 0;
+	}
+	if (g_signal < 0)
+		g_signal *= -1;
+	kill(g_signal, SIGUSR1);
+	g_signal = 0;
 }
 
 int	main(void)
 {
-	struct sigaction	act1;
+	struct sigaction	act;
 
-	act1.sa_sigaction = server_handler;
-	act1.sa_flags = SA_SIGINFO;
-	write(1, "server PID: ", 12);
+	act.sa_sigaction = &server_handler;
+	act.sa_flags = SA_SIGINFO;
+	write(1, "server PID : ", 13);
 	ft_putnbr_fd(getpid(), 1);
-	if (sigaction(SIGUSR1, &act1, NULL) != 0)
+	write(1, "\n", 1);
+	g_signal = 0;
+	if (sigaction(SIGUSR1, &act, NULL) != 0)
 	{
 		write(2, "sigcation error\n", 16);
 		return (1);
 	}
-	if (sigaction(SIGUSR2, &act1, NULL) != 0)
+	if (sigaction(SIGUSR2, &act, NULL) != 0)
 	{
 		write(2, "sigcation error\n", 16);
 		return (1);
 	}
 	while (1)
-		pause();
+		server_loop();
 	return (0);
 }
